63e72ad75afb58253825d0b5f285c3c26b36d4c6
MitchelTzk <mitchel.tzk@gmail.com>
2022-03-16 20:16:14 +0800

    Changed normWeight to termWeight in Node.py, added information to README.txt, and commented out a print statement and unnecessary functions in search.py

--- search.py
+++ search.py
@@ -25,14 +25,6 @@ def run_search(dict_file, postings_file, queries_file, results_file):
     dictFile = TermDictionary(dict_file)
     dictFile.load()  # load term information into dictFile from dict_file
 
-    # with open(queries_file, 'r') as queryFile:
-    #     with open(results_file, 'w') as resultFile:
-    #         allResult = []
-    #         for query in queryFile:
-    #             queryVector = getQueryVector(query, dictFile, postings_file)
-    #         outputResult = "\n".join(allResult)  # to output all result onto a new line.
-    #         resultFile.write(outputResult)
-
     with open(queries_file, 'r') as queryFile:
         with open(results_file, 'w') as resultFile:
             
@@ -56,59 +48,59 @@ def retrievePostingsList(file, pointer):
     return postingsList
 
 
-def getQueryVector(query, dictFile, postingsFile):
-    """
-    returns a vector of the ltc scores of each term
-    """
-    tfidf_scores = []
-    ltc_scores = []
-    queryTerms = []
-    stemmer = nltk.stem.porter.PorterStemmer()
-
-    query = nltk.tokenize.word_tokenize(query)
-    for term in query:
-        term = stemmer.stem(term.lower())
-        if term in queryTerms:
-            continue
-        else:
-            queryTerms.append(term)
-            tfidf_scores.append(get_tf_idf(term, query, dictFile, postingsFile))  # get tf_idf weights according to queryTerms
-    for score in tfidf_scores:
-        ltc_scores.append(normalise(score, tfidf_scores))
-
-    return ltc_scores
-
-
-def get_tf_idf(term, query, dictFile, postingsFile):
-    """
-    gets the tf-idf weight of the term
-    """
-    tf = 0
-    for word in query:
-        if term == word:
-            tf += 1
-    tf_wt = 1 + math.log10(tf)  # get logarithmic tf
-
-    pointer = dictFile.getTermPointer(term)
-    postings = retrievePostingsList(postingsFile, pointer)
-    docFreq = len(postings)
-    corpusPointer = dictFile.getPointerToCorpusDocIDs()
-    totalDocs = len(retrievePostingsList(postingsFile, corpusPointer))
-    idf = math.log10(totalDocs/docFreq)     # get idf
-
-    return tf_wt * idf
-
-
-def normalise(tf_idf, scores):
-    """
-    returns normalised score
-    """
-    total = 0
-    for score in scores:
-        total += score**2
-    result = math.sqrt(total)
-
-    return 1/result * tf_idf
+# def getQueryVector(query, dictFile, postingsFile):
+#     """
+#     returns a vector of the ltc scores of each term
+#     """
+#     tfidf_scores = []
+#     ltc_scores = []
+#     queryTerms = []
+#     stemmer = nltk.stem.porter.PorterStemmer()
+#
+#     query = nltk.tokenize.word_tokenize(query)
+#     for term in query:
+#         term = stemmer.stem(term.lower())
+#         if term in queryTerms:
+#             continue
+#         else:
+#             queryTerms.append(term)
+#             tfidf_scores.append(get_tf_idf(term, query, dictFile, postingsFile))  # get tf_idf weights according to queryTerms
+#     for score in tfidf_scores:
+#         ltc_scores.append(normalise(score, tfidf_scores))
+#
+#     return ltc_scores
+#
+#
+# def get_tf_idf(term, query, dictFile, postingsFile):
+#     """
+#     gets the tf-idf weight of the term
+#     """
+#     tf = 0
+#     for word in query:
+#         if term == word:
+#             tf += 1
+#     tf_wt = 1 + math.log10(tf)  # get logarithmic tf
+#
+#     pointer = dictFile.getTermPointer(term)
+#     postings = retrievePostingsList(postingsFile, pointer)
+#     docFreq = len(postings)
+#     corpusPointer = dictFile.getPointerToCorpusDocIDs()
+#     totalDocs = len(retrievePostingsList(postingsFile, corpusPointer))
+#     idf = math.log10(totalDocs/docFreq)     # get idf
+#
+#     return tf_wt * idf
+#
+#
+# def normalise(tf_idf, scores):
+#     """
+#     returns normalised score
+#     """
+#     total = 0
+#     for score in scores:
+#         total += score**2
+#     result = math.sqrt(total)
+#
+#     return 1/result * tf_idf
 
 
 def cosineScores(query, dictionary, postingsFile):
@@ -122,7 +114,7 @@ def cosineScores(query, dictionary, postingsFile):
     queryTokens = [stemmer.stem(token.lower()) for token in query.split()]
     qTokenFrequency = Counter(queryTokens) # {"the": 2, "and" : 1}
     qToken_tfidfWeights = {term : computeTFIDF(term, frequency, dictionary, totalNumberOfDocs) for term, frequency in qTokenFrequency.items()}
-    print(qToken_tfidfWeights)
+    # print(qToken_tfidfWeights)
     queryLength = math.sqrt(sum([math.pow(weight, 2) for weight in qToken_tfidfWeights.values()]))
     qTokenNormalisedWeights = {term : weight/queryLength for term, weight in qToken_tfidfWeights.items()}
 
